8b7391c7d8e6101a04a243bd02439ad960009217
varunbhandia <varunbhandia@gmail.com>
2020-08-12 23:01:57 +0530

    tasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,36 @@ var styles = [
 var styles1 = [
     'font-family: raleway',
     'color: blue',
-    'font-size: 10px',
+    'font-size: 20px',
 ].join(';');
 
 console.log('%c Standard Chartered Tasks', styles);
-console.log('%c =>Fi', styles1);
+
+function fibonacci(num) {
+    var num1 = 0;
+    var num2 = 1;
+    var sum;
+    var i = 0;
+    console.log(num1);
+    console.log(num2);
+    for (i = 0; i < num - 2; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        console.log(num2);
+    }
+}
+
+function makeTable(num, limit) {
+    console.log('-----------------');
+    for (i = 1; i < limit; i++) {
+        console.log('|', i, 'X', num, '|', '= |', num * i, '|');
+    }
+    console.log('-----------------');
+
+}
+console.log('%c => Fibonacci Series', styles1);
+fibonacci(10);
+
+console.log('%c => Display Table', styles1);
+makeTable(8, 10)
