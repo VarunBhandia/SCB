f9be468884c353c7d7ec8e03bdf46774e1ba5f7c
varunbhandia <varunbhandia@gmail.com>
2020-08-12 22:50:26 +0530

    Add | JS Code

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var styles = [
+    'color: red',
+    'font-wieght: 900',
+    'font-size: 25px',
+    'font-family: "Raleway, sans-serif"',
+].join(';');
+var styles1 = [
+    'font-family: raleway',
+    'color: blue',
+    'font-size: 10px',
+].join(';');
+
+console.log('%c Standard Chartered Tasks', styles);
+console.log('%c =>Fi', styles1);
